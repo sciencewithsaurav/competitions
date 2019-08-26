df8678a047a2bbf81165f041bc158064f3921abd
sciencewithsaurav <53606230+sciencewithsaurav@users.noreply.github.com>
2019-08-26 23:06:43 +0530

    Import and rudimentary EDA

--- WNS Analytics Code.R
+++ WNS Analytics Code.R
@@ -1,4 +1,8 @@
 
+###############################
+#  Packages import
+###############################
+
 library('ggplot2') # visualization
 library('ggthemes') # visualization
 library('scales') # visualization
@@ -6,7 +10,9 @@ library('dplyr') # data manipulation
 library('mice') # imputation
 library('randomForest') # classification algorithm
 
-#Now that our packages are loaded, let's read in and take a peek at the data.
+###############################
+#  Importing data
+###############################
 
 #view log of users (2018/10/15 - 2018/12/11)
 view_log <- read.csv('D:/Data Science/WNS Analytics Wizard 2019/train_NA17Sgz/view_log.csv', stringsAsFactors = F)
@@ -20,6 +26,25 @@ head(item_data)
 train <- read.csv('D:/Data Science/WNS Analytics Wizard 2019/train_NA17Sgz/train.csv', stringsAsFactors = F)
 head(train)
 
+
+###############################
+#  Exploratory data analysis
+###############################
+
+unique(view_log$device_type) #"android" "iphone"  "web" 
+
+count(view_log, view_log$device_type)
+#1 android                3117737
+#2 iphone                     861
+#3 web                         24
+
+unique(train$os_version) #"old"          "latest"       "intermediate"
+
+ggplot(train,aes(x = train$os_version, fill = factor(train$is_4G))) +
+  geom_bar(stat='count') +
+  scale_y_continuous(trans = "log10") +
+  xlab("OS version") +
+  facet_grid(.~train$is_click)
 
 
 
